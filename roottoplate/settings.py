b8a8ac4491365c699d09ce1284543fbaa6782d54
Daniel Tudose <2569281t@student.gla.ac.uk>
2022-11-02 13:47:14 +0000

    again

--- roottoplate/settings.py
+++ roottoplate/settings.py
@@ -113,8 +113,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
-STATIC_URL='/static/'
 
 
 # Default primary key field type
