e83990eb8a5650405b728a890e539c1bb999b6b8
“Farzwan <2553017m@student.gla.ac.uk>
2022-11-23 12:41:13 +0000

    restart django

--- roottoplate/composter/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComposterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'composter'
--- roottoplate/composter/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from composter import views
-
-app_name='composter'
-urlpatterns=[
-    path('',views.index,name='index'),
-]
