b7d6e32a2b9093c734479f626ea04cf80c099cb0
Daniel <dntudose@gmail.com>
2022-11-24 15:38:23 +0000

    update settings and views.py

--- roottoplate/roottoplate/urls.py
+++ roottoplate/roottoplate/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from composter import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('composter.urls'))
+    path('' ,include('composter.urls')),
+    path('composter/',include('composter.urls'))
+
 ]
