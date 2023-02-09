87877ea456a17586e8824913c95063651d46bbb3
“Farzwan <2553017m@student.gla.ac.uk>
2023-02-09 19:46:49 +0000

    fix csrf

--- roottoplate/roottoplate/settings.py
+++ roottoplate/roottoplate/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
-
+CSRF_TRUSTED_ORIGINS=['roottoplate.herokuapp.com']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
