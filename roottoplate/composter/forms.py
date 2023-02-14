e9f84ac8b6de2e0b23acc314acaf356f74ba8e51
sandm <2309647m@student.gla.ac.uk>
2023-02-14 17:40:30 +0000

    flake8

--- roottoplate/composter/forms.py
+++ roottoplate/composter/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from composter.models import InputType, InputEntry, Input, TemperatureEntry, RestaurantRequest, Output, EnergyUsage
-from datetime import date, datetime, timedelta
+from datetime import date
 import datetime
 
 
