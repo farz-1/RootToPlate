936a75f63cd69805ed1ed342245ccdf6cb46afff
2560822h <2560822h@student.gla.ac.uk>
2023-03-02 15:42:36 +0000

    logic of composter advice in views. related to #110, #111

--- roottoplate/composter/views.py
+++ roottoplate/composter/views.py
@@ -189,8 +189,26 @@ class InputFormView(TemplateView):
 
         entry_form = InputEntryForm(self.request.POST)
         input_formset = InputFormSet(self.request.POST)
+        context = {}
 
-        if entry_form.is_valid() and input_formset.is_valid():
+        if 'get_advice' in self.request.POST:
+            cur_inputs = []
+            for input in input_formset:
+                cur_inputs.append({'amount': input.inputAmount, 'type': InputType.objects.filter(name=input.inputType)})
+            sumC, sumN = calculate_mixture_sums(cur_inputs)
+            if sumC/sumN > 35:
+                rec_input = InputType.objects.filter(name='Food waste')
+                rec_input_amount = calculate_recommended_addition(rec_input, sumC, sumN)
+                advice = f"The carbon-nitrogen ratio of this mixture is too high. Recommended addition: roughly {rec_input_amount} of green material."
+            elif sumC/sumN < 20:
+                rec_input = InputType.objects.filter(name='Woodchips')
+                rec_input_amount = calculate_recommended_addition(rec_input, sumC, sumN)
+                advice = f"The carbon-nitrogen ratio of this mixture is too low. Recommended addition: roughly {rec_input_amount} of brown material."
+            else:
+                advice = f"The carbon-nitrogen ratio is within the recommended range."
+            context['advice'] = advice
+
+        elif entry_form.is_valid() and input_formset.is_valid():
             entry = entry_form.save(commit=False)
             entry.user = user
             entry.save()
@@ -203,7 +221,9 @@ class InputFormView(TemplateView):
             print(entry_form.errors)
             for inputs in input_formset:
                 print(inputs.errors)
-        context = {'input_formset': input_formset, 'entry_form': entry_form}
+
+        context['input_formset'] = input_formset
+        context['entry_form'] = entry_form
         return render(request, self.template_name, context)
 
 
