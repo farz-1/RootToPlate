a8ae864c1be76bbdec3b95ce74dcb84c60954f3b
Farzwan Mohamed <2553017m@student.gla.ac.uk>
2023-03-08 13:38:15 +0000

    Update settings.py

--- roottoplate/roottoplate/settings.py
+++ roottoplate/roottoplate/settings.py
@@ -69,11 +69,8 @@ WSGI_APPLICATION = 'roottoplate.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'er': dj_database_url.config()
+    
+    'default': dj_database_url.config()
 }
 
 # Password validation
